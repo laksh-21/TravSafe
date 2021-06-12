4cc6a0db0f0d232cde7aed0c2865d11f9c847326
laksh-21 <iamlakshable@gmail.com>
2021-06-12 08:56:29 +0530

    Add Navigation Architecture

--- app/src/main/java/com/example/travsafe/MainActivity.kt
+++ app/src/main/java/com/example/travsafe/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.travsafe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun findNavController(): NavController{
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        return navHostFragment.navController
     }
 }
