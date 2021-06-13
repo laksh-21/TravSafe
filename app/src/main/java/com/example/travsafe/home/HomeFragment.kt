2a9f37efda2440befb04d461ecd6d00d22953378
laksh-21 <iamlakshable@gmail.com>
2021-06-13 11:05:45 +0530

    Fix Gradle Issue

--- app/src/main/java/com/example/travsafe/home/HomeFragment.kt
+++ app/src/main/java/com/example/travsafe/home/HomeFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.ui.platform.ComposeView
+import com.example.travsafe.home.components.HomeAppBar
 
 class HomeFragment : Fragment() {
     override fun onCreateView(
@@ -15,7 +17,13 @@ class HomeFragment : Fragment() {
     ): View {
         return ComposeView(requireContext()).apply {
             setContent {
-                Text(text = "Hello!")
+                Scaffold(
+                    topBar = {
+                        HomeAppBar("Title here boi")
+                    }
+                ){
+                    Text(text = "Hello!")
+                }
             }
         }
     }
